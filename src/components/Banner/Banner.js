cbe4a7f7e516ccb32193939b1a5c8dd845e85fc2
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-12-24 09:51:27 -0500

    create banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './Banner.module.scss';
+
+const Banner = (props) => {
+    return (
+        <div className={classes.banner__container}>
+            <div className={classes.banner__user}>
+                <img src="https://images.pexels.com/photos/2527491/pexels-photo-2527491.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="user_image" className={classes.banner__image} />
+            </div>
+            <div className={classes.banner__welcome}>
+                <p className={classes.banner__name}>Sebastian Rivera Giraldo</p>
+                <p className={classes.banner__message}>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Banner from '../Banner/Banner'
 import MainNav from '../MainNav/MainNav'
 
 const Header = () => {
     return (
         <header>
             <MainNav></MainNav>
+            <Banner></Banner>
         </header>
     )
 }
