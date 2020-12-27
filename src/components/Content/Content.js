51ee375c86d32f30c00558a9c4ec1d2ad2073d23
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-12-27 15:36:53 -0500

    create actived function

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,40 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Classes from './Content.module.scss';
 
 const Content = () => {
+
+    const[states, setStates] = useState([true, false, false, false, false]);
+
+    const activated = (e, element) => {
+        e.preventDefault();
+        const newStates = [false, false, false, false, false];
+
+        for (const index in newStates) {
+            Number(index) === element ? newStates[index] = true : newStates[index] = false;
+        }
+        setStates(newStates);
+    }
+
+    console.log(states);
+    
     return (
         <div className={Classes.content__container}>
             <ul className={Classes.content__nav}>
-                <li className={Classes.content__item}>
+                <li className={`${Classes.content__item} ${states[0] ? Classes.active : ''}`} onClick={(e) => activated(e, 0)}>
                     <a href="#" className={Classes.content__link}>
                         <span className={Classes.content__name}>
                             Item 1
                         </span>
                     </a>
                 </li>
-                <li className={Classes.content__item}>
+                <li className={`${Classes.content__item} ${states[1] ? Classes.active : ''}`} onClick={(e) => activated(e, 1)}>
                     <a href="#" className={Classes.content__link}>
                         <span className={Classes.content__name}>
                             Item 2
                         </span>
                     </a>
                 </li>
-                <li className={Classes.content__item}>
+                <li className={`${Classes.content__item} ${states[2] ? Classes.active : ''}`} onClick={(e) => activated(e, 2)}>
                     <a href="#" className={Classes.content__link}>
                         <span className={Classes.content__name}>
                             Item 3
                         </span>
                     </a>
                 </li>
-                <li className={Classes.content__item}>
+                <li className={`${Classes.content__item} ${states[3] ? Classes.active : ''}`} onClick={(e) => activated(e, 3)}>
                     <a href="#" className={Classes.content__link}>
                         <span className={Classes.content__name}>
                             Item 4
                         </span>
                     </a>
                 </li>
-                <li className={Classes.content__item}>
+                <li className={`${Classes.content__item} ${states[4] ? Classes.active : ''}`} onClick={(e) => activated(e, 4)}>
                     <a href="#" className={Classes.content__link}>
                         <span className={Classes.content__name}>
                             Item 5
